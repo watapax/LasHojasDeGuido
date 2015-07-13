9524ee6118bc09c8f68c3e174a836cc53d158d72
Juan Correa <wuazopax@gmail.com>
2015-07-13 15:14:54 -0400

    Agregar Proyecto Inicial

---
+++ Las hojas de Guido/Assets/Scripts/Tool/AnimationFunction.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Clase solo para funciones de animaciones
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class AnimationFunction : MonoBehaviour {
+
+
+	AudioSource audioS;
+	Animator anim;
+
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+
+	// REPRODUCE UN SONDIDO
+	public void PlaySound(AudioClip audioclip)
+	{
+		// chequea si exist el componente AudioSource
+		if(gameObject.GetComponent<AudioSource>() == null)
+			audioS =  gameObject.AddComponent<AudioSource>();
+
+		audioS.PlayOneShot(audioclip);
+	}
+
+
+	// REPRODUCE UNA ANIMACION CON TRIGGER
+	public void PlayAnimation(string triggerName)
+	{
+		anim.SetTrigger(triggerName);
+	}
+
+
+}
